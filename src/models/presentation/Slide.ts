62eb4c2c1ac09c9530dd2450d12a473443e4b2b2
Dmitriy Ivanov <daug32@mail.ru>
2022-12-15 22:14:23 +0300

    fix: Поправил функции

--- src/models/presentation/Slide.ts
+++ src/models/presentation/Slide.ts
@@ -2,33 +2,10 @@ import { Attachment } from "./Attachment";
 import { AttachmentType } from "./AttachmentType";
 
 export class Slide {
-    private _background: Attachment;
-
     constructor( 
         public id: number, 
         public attachments: Attachment[],
-        public position: number
-    ) {
-        this._background = new Attachment(
-            -1,
-            'white',
-            AttachmentType.Shape
-        );
-    }
-
-    public getBackgound(): string {
-        return this._background.data;
-    }
-
-    public setBackground(backgound: string) {
-        this._background.data = backgound;
-    }
-
-    public getCopy(): Slide {
-        return new Slide(
-            this.id,
-            [...this.attachments],
-            this.position 
-        );
-    }
+        public orderPosition: number,
+        public backgroundColor: string = "white"
+    ) { }
 }
